516e5b39748faf25f4a299111e5f93f61e227221
Tom Rowson <tom.rowson@gmail.com>
2018-10-19 12:30:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(array, name)
   array.push("name")
-  place = array.length + 1
+  place = array.length
  puts "Welcome, #{name}. You are number #{place} in line." 
 end
   
